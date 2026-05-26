f5006c78bdda7f9257b876be176127c846b2dae9
esintao <esintao@hotmail.com>
2026-05-26 08:46:26 +0200

    config and db for setting up psycopg2

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    # Database connection parameters
+    DB_HOST = 'localhost'
+    DB_PORT = 5432
+    DB_NAME = 'dynamic_dining'
+    DB_USER = 'postgres'
+    DB_PASSWORD = 'password'
